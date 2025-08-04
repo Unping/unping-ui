886d06632f9af95992f2f10d86614552f40fa4f5
Joern Depenbrock <joern@j-d-pictures.de>
2025-08-04 06:14:50 +0200

    - added outline button examples

--- widgetbook/lib/base_button.dart
+++ widgetbook/lib/base_button.dart
@@ -215,12 +215,209 @@ Widget buildBaseButtonFilled(BuildContext context) {
 
 @widgetbook.UseCase(name: 'Outline', type: BaseButton)
 Widget buildBaseButtonOutlined(BuildContext context) {
-  return Container(
-    padding: const EdgeInsets.all(16),
-    child: Buttons.outline(
-      text: 'Button',
-      onPressed: () {},
-    ),
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+      ]),
+      //Hovered state
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            forceState: ButtonState.hovered
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+            forceState: ButtonState.hovered
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.hovered
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.hovered
+          ),
+        ),
+      ]),
+      //Focused state
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            forceState: ButtonState.focused
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+            forceState: ButtonState.focused
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.focused
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.focused
+          ),
+        ),
+      ]),
+      //Disabled state
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: null,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: null,
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            text: 'Button',
+            onPressed: null,
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.outline(
+            onPressed: null,
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+      ]),
+    ],
   );
 }
 
