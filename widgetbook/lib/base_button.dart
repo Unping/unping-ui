fb17336ed20ce5098b245ea704db0fb995c322df
Joern Depenbrock <joern@j-d-pictures.de>
2025-08-04 06:09:50 +0200

    - added some filled button examples in same grid as figma file

---
+++ widgetbook/lib/base_button.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+// Import the widget from your app
+import 'package:unping_ui/unping_ui.dart';
+
+@widgetbook.UseCase(name: 'Filled', type: BaseButton)
+Widget buildBaseButtonFilled(BuildContext context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+      ]),
+      //Hovered state
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            forceState: ButtonState.hovered
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+            forceState: ButtonState.hovered
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.hovered
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.hovered
+          ),
+        ),
+      ]),
+      //Focused state
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            forceState: ButtonState.focused
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+            forceState: ButtonState.focused
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.focused
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            onPressed: () {},
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+            forceState: ButtonState.focused
+          ),
+        ),
+      ]),
+      //Disabled state
+      Row(children: [
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: null,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: null,
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.leading,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            text: 'Button',
+            onPressed: null,
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(16),
+          child: Buttons.filled(
+            onPressed: null,
+            icon: Icon(
+              Icons.radio_button_unchecked,
+              size: 16,
+              color: Color(0xFF2A313C), // Match the text color
+            ),
+            iconPosition: IconPosition.trailing,
+          ),
+        ),
+      ]),
+
+    ],
+  );
+}
+
+@widgetbook.UseCase(name: 'Outline', type: BaseButton)
+Widget buildBaseButtonOutlined(BuildContext context) {
+  return Container(
+    padding: const EdgeInsets.all(16),
+    child: Buttons.outline(
+      text: 'Button',
+      onPressed: () {},
+    ),
+  );
+}
+
+//TODO: see figma file, provide all variants
+@widgetbook.UseCase(name: 'Ghost', type: BaseButton)
+Widget buildBaseButtonTextOnly(BuildContext context) {
+  return Container(
+    padding: const EdgeInsets.all(16),
+    child: Buttons.ghost(
+      text: 'Button',
+      onPressed: () {},
+    ),
+  );
+}
+
--- widgetbook/lib/main.directories.g.dart
+++ widgetbook/lib/main.directories.g.dart
@@ -11,14 +11,32 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:widgetbook/widgetbook.dart' as _i1;
-import 'package:widgetbook_workspace/cool_button.dart' as _i2;
+import 'package:widgetbook_workspace/base_button.dart' as _i2;
+import 'package:widgetbook_workspace/cool_button.dart' as _i3;
 
 final directories = <_i1.WidgetbookNode>[
+  _i1.WidgetbookComponent(
+    name: 'BaseButton',
+    useCases: [
+      _i1.WidgetbookUseCase(
+        name: 'Filled',
+        builder: _i2.buildBaseButtonFilled,
+      ),
+      _i1.WidgetbookUseCase(
+        name: 'Ghost',
+        builder: _i2.buildBaseButtonTextOnly,
+      ),
+      _i1.WidgetbookUseCase(
+        name: 'Outline',
+        builder: _i2.buildBaseButtonOutlined,
+      ),
+    ],
+  ),
   _i1.WidgetbookLeafComponent(
     name: 'CoolButton',
     useCase: _i1.WidgetbookUseCase(
       name: 'Default',
-      builder: _i2.buildCoolButtonUseCase,
+      builder: _i3.buildCoolButtonUseCase,
     ),
-  )
+  ),
 ];
