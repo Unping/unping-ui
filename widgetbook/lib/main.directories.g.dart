706ca9349c5706263941a777778ded91b2fff691
Joern Depenbrock <joern@j-d-pictures.de>
2025-08-05 05:46:26 +0200

    - riggered directories generation for widgetbook

--- widgetbook/lib/main.directories.g.dart
+++ widgetbook/lib/main.directories.g.dart
@@ -11,9 +11,7 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:widgetbook/widgetbook.dart' as _i1;
-<<<<<<< HEAD
 import 'package:widgetbook_workspace/base_button.dart' as _i2;
-import 'package:widgetbook_workspace/cool_button.dart' as _i3;
 
 final directories = <_i1.WidgetbookNode>[
   _i1.WidgetbookComponent(
@@ -32,16 +30,5 @@ final directories = <_i1.WidgetbookNode>[
         builder: _i2.buildBaseButtonOutlined,
       ),
     ],
-  ),
-  _i1.WidgetbookLeafComponent(
-    name: 'CoolButton',
-    useCase: _i1.WidgetbookUseCase(
-      name: 'Default',
-      builder: _i3.buildCoolButtonUseCase,
-    ),
-  ),
+  )
 ];
-=======
-
-final directories = <_i1.WidgetbookNode>[];
->>>>>>> main
