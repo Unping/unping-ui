47f212a323b51a61e1a02bb3b91d1c36b83ce243
Joern Depenbrock <joern@j-d-pictures.de>
2025-08-30 05:40:40 +0200

    - adjusted main and integrated an introduction file, based on README.md from main project

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -1,25 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+import 'package:widgetbook_workspace/introduction.dart';
+import 'package:unping_ui/unping_ui.dart';
 
 // This file does not exist yet,
 // it will be generated in the next step
 import 'main.directories.g.dart';
 
-void main() {
-  runApp(const WidgetbookApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var readme = '';
+  try {
+    readme = await rootBundle.loadString('assets/README_UNPING_UI_COPIED.md');
+  } catch (e) {
+    debugPrint('Cannot read README.md: $e');
+  } finally {
+    runApp(WidgetbookApp(readme: readme));
+  }
 }
 
 @widgetbook.App()
 class WidgetbookApp extends StatelessWidget {
-  const WidgetbookApp({super.key});
-
+  const WidgetbookApp({super.key, this.readme = ''});
+  
+  final String readme;
+  
   @override
   Widget build(BuildContext context) {
-    return Widgetbook.material(
+    return Widgetbook(
+      appBuilder: (context, child) => MaterialApp(
+        title: 'Unping UI Widgetbook',
+        theme: UiTheme.lightTheme(),
+        darkTheme: UiTheme.darkTheme(),
+        home: child,
+        debugShowCheckedModeBanner: false,
+        builder: (context, child) {
+          return DefaultTextStyle(
+            style: const TextStyle(decoration: TextDecoration.none),
+            child: child ?? const SizedBox.shrink(),
+          );
+        },
+      ),
       // The [directories] variable does not exist yet,
       // it will be generated in the next step
-      directories: directories,
+      directories: [
+        WidgetbookUseCase(
+          name: 'Introduction',
+          builder: (BuildContext context) => IntroductionWidgetbook(readme: readme),
+        ),
+        // Find and place Foundation directory as second entry
+        ...directories.where((dir) => dir.name == 'Foundation'),
+        // Add all other directories except Foundation
+        ...directories.where((dir) => dir.name != 'Foundation'),
+      ],
     );
   }
 }
